89f82662a500c5f427c05d860a301548c15d94e4
EatPrilosec <ae3nerdgod@gmail.com>
2025-06-17 04:53:42 -0400

    Update hwrev.c

--- cmd/hwrev.c
+++ cmd/hwrev.c
@@ -22,7 +22,8 @@ int do_hwrev(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	/* RG351MP */
 	if (check_range(146, 186, hwrev_adc)) {
 		env_set("hwrev", "rg351mp");
-		run_command("setenv -f dtb_name \"${PanelPathSlash}rg351mp-kernel.dtb\"", 0);
+		/* run_command("setenv -f dtb_name \"${PanelPathSlash}rg351mp-kernel.dtb\"", 0); */
+		run_command("setenv -f dtb_name \"rg351mp-kernel.dtb\"", 0);
 		run_command("setenv -f PanChoEnabled yes", 0); 
 
 	}
