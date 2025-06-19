58692fa9b98ee7734b8f9c7e65ebcbc54b8ce993
EatPrilosec <ae3nerdgod@gmail.com>
2025-06-19 06:00:05 -0400

    Update hwrev.c

--- cmd/hwrev.c
+++ cmd/hwrev.c
@@ -22,7 +22,11 @@ int do_hwrev(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	/* RG351MP */
 	if (check_range(146, 186, hwrev_adc)) {
 		env_set("hwrev", "rg351mp");
-		run_command("setenv -f PanChoEnvSize 0x1000; setenv -f PanChoEnvLoc 28560", 0); /* seems clear on uboot.bin mbr and gpt */
+		run_command("mmc dev 1:1", 0);
+		run_command("printenv; sleep 20", 0);
+		
+		
+		 /* run_command("setenv -f PanChoEnvSize 0x1000; setenv -f PanChoEnvLoc 28560", 0); seems clear on uboot.bin mbr and gpt
 		run_command("setenv -f WipePanChoEnv \"mmc erase ${PanChoEnvLoc} ${PanChoEnvSize}\";", 0); 
 		run_command("setenv -f LoadPanChoEnv \"mmc read 0x2000000 ${PanChoEnvLoc} ${PanChoEnvSize};env import -b 0x2000000 ${PanChoEnvSize}\";", 0); 
 		run_command("setenv -f SavePanChoEnv \"env export -b -s ${PanChoEnvSize} 0x2000000; mmc write 0x2000000 ${PanChoEnvLoc} ${PanChoEnvSize}\"", 0); 
@@ -32,7 +36,7 @@ int do_hwrev(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		
 		run_command("if ${PanChoEnvLoaded}; then; echo PanChoEnv Loaded; else; if load mmc 1:1 0x02000000 rg351mp-kernel.dtb; then; echo \"found rg351mp-kernel.dtb, business as usual\"; else; echo \"rg351mp-kernel.dtb not, uninitialized PanCho?\"; if load mmc 1:1 0x02000000 \"ScreenFiles/Panel 4/rg351mp-kernel.dtb\"; then; echo \"found ScreenFiles, lets temporarily load default (pan 4) so PanCho can init\"; setenv PanelPathSlash \"ScreenFiles/Panel 4/\"; else; echo \"cant find r36s dtb anywhere expected, boot will fail so lets fail early and sleep for a while to avoid lots of looping\"; sleep 60; reset; fi; fi; fi", 0);
 		
-		run_command("setenv -f dtb_name \"${PanelPathSlash}rg351mp-kernel.dtb\"", 0);
+		run_command("setenv -f dtb_name \"${PanelPathSlash}rg351mp-kernel.dtb\"", 0); */
 	}
 	/* RG351V */
 	else if (check_range(494, 534, hwrev_adc)) {
