12ab34eacc474a04fb8da59f967ea0f4e4969555
EatPrilosec <ae3nerdgod@gmail.com>
2025-06-20 05:33:28 -0400

    Update board.c

--- arch/arm/mach-rockchip/board.c
+++ arch/arm/mach-rockchip/board.c
@@ -398,22 +398,9 @@ int init_kernel_dtb(void)
 		if (ret != CMD_RET_SUCCESS) {
 			printf("dtb in spi flash fail, try dtb in fat\n");
 			/* just find a temp dtb somewhere */
-			setenv dtbfound false
-			for dtbfile in rg351mp-kernel.dtb rg351mp-uboot.dtb r3xs-uboot.dtb r36s-uboot.dtb
-			do
-				if load mmc 1:1 ${fdt_addr_r} \"${dtbfile}\"
-				then
-					echo \"found dtb on root\"
-				elif load mmc 1:1 ${fdt_addr_r} \"ScreenFiles/${dtbfile}\"
-				then
-					echo \"found dtb in ScreenFiles\"
-				elif load mmc 1:1 ${fdt_addr_r} \"ScreenFiles/Panel 0/${dtbfile}\"
-				then
-					echo \"found dtb in ScreenFiles/Panel 0\"
-				fi
-			done
+			run_command("setenv dtbfound false; for dtbfile in rg351mp-kernel.dtb rg351mp-uboot.dtb r3xs-uboot.dtb R3XS-uboot.dtb r36s-uboot.dtb R36S-uboot.dtb; do; if ${dtbfound}; then; echo already found dtb; else; if load mmc 1:1 ${fdt_addr_r} \"${dtbfile}\"; then; echo \"found dtb on root\"; setenv dtbfound true; elif load mmc 1:1 ${fdt_addr_r} \"ScreenFiles/${dtbfile}\"; then; echo \"found dtb in ScreenFiles\"; setenv dtbfound true; elif load mmc 1:1 ${fdt_addr_r} \"ScreenFiles/Panel 0/${dtbfile}\"; then; echo \"found dtb in ScreenFiles/Panel 0\"; setenv dtbfound true; fi; fi;done", 0);
 			
-			ret = run_command("load mmc 1:1 ${fdt_addr_r} \"${dtb_name}\"", 0);
+			/* ret = run_command("load mmc 1:1 ${fdt_addr_r} \"${dtb_name}\"", 0); */
 
 			
 			if (CMD_RET_SUCCESS != check_fdt_header(fdt_addr)) {
