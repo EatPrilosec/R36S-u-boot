d68a1e90b8182210e41227e5cc60c9e29a6373e4
EatPrilosec <ae3nerdgod@gmail.com>
2025-06-20 09:27:15 -0400

    Update hwrev.c

--- cmd/hwrev.c
+++ cmd/hwrev.c
@@ -22,8 +22,8 @@ int do_hwrev(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	/* RG351MP */
 	if (check_range(146, 186, hwrev_adc)) {
 		env_set("hwrev", "rg351mp");
-		run_command("if load mmc 1:1 0x2000000 logo.env; then; env import -t 0x2000000 ${filesize}; fi", 0);
-		env_set("dtb_name", "${PanelPathSlash}rg351mp-kernel.dtb");
+		run_command("if load mmc 1:1 0x2000000 logo.env; then; env import -t 0x2000000 ${filesize}; fi; setenv dtb_name \"${PanelPathSlash}rg351mp-kernel.dtb\"", 0);
+		/*env_set("dtb_name", "${PanelPathSlash}rg351mp-kernel.dtb");*/
 	}
 	/* RG351V */
 	else if (check_range(494, 534, hwrev_adc)) {
