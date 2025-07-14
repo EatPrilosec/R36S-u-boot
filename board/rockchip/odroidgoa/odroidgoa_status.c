5b0ae5d616b75160a7f8b991189870c26511719d
EatPrilosec <ae3nerdgod@gmail.com>
2025-07-13 21:02:39 -0400

    Update odroidgoa_status.c

--- board/rockchip/odroidgoa/odroidgoa_status.c
+++ board/rockchip/odroidgoa/odroidgoa_status.c
@@ -156,8 +156,8 @@ int odroid_display_status(int logo_mode, int logo_storage, const char *str)
 		// run_command(cmd, 0);
 		run_command("mw.b 0x3df2c000 0 0x1000; echo \"Loading logo\"; if load mmc 1:1 0x3df2c000 \"${PanelPathSlash}logo.bmp\"; then; echo \"Showing logo\"; show_bmp 0x3df2c000; else; echo \"logo load fail\"; true; fi", 0);
 
-		if (show_bmp(bmp_mem))
-			printf("[%s] show_bmp Fail!\n", __func__);
+		// if (show_bmp(bmp_mem))
+		// 	printf("[%s] show_bmp Fail!\n", __func__);
 		break;
 	case LOGO_STORAGE_ANYWHERE:
 	default:
@@ -182,8 +182,8 @@ int odroid_display_status(int logo_mode, int logo_storage, const char *str)
 			// run_command(cmd, 0);
 			run_command("mw.b 0x3df2c000 0 0x1000; echo \"Loading logo\"; if load mmc 1:1 0x3df2c000 \"${PanelPathSlash}logo.bmp\"; then; echo \"Showing logo\"; show_bmp 0x3df2c000; else; echo \"logo load fail\"; true; fi", 0);
 
-			if (show_bmp(bmp_mem))
-				printf("[%s] show_bmp Fail!\n", __func__);
+			// if (show_bmp(bmp_mem))
+			// 	printf("[%s] show_bmp Fail!\n", __func__);
 		}
 		break;
 	}
